a41a4eccc9a092ddcf64d9c01124c69fa17d333c
me-adi <aditya.p23@iiits.in>
2024-07-14 15:50:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,37 +21,37 @@ document.addEventListener("keydown",function (event) {
 function makeSound(key) {
     
     switch (key) {
-        case a:
+        case "a":
             var snare = new Audio("./sounds/snare.mp3");
             snare.play();
             break;
 
-        case s:
+        case "s":
             var tom1 = new Audio("./sounds/tom-1.mp3");
             tom1.play();
             break;
 
-        case d:
+        case "d":
             var tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
 
-        case f:
+        case "f":
             var tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();
             break;
 
-        case j:
+        case "j":
             var tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
 
-        case k:
+        case "k":
             var kickbass = new Audio("sounds/kick-bass.mp3");
             kickbass.play();
             break;
 
-        case l:
+        case "l":
             var crash = new Audio("sounds/crash.mp3");
             crash.play();
             break;
